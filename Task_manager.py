a696864e30d581665443b4bfd540173887834d95
Antonio <antonpogoryelov@gmail.com>
2024-06-27 11:36:32 +0300

    Create class Task
    Class with methods was created

---
+++ Task_manager.py
@@ -0,0 +1,28 @@
+class Task():
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, description, deadline):
+        task_exist = False
+        for task in self.tasks:
+            if task['description'] == description: task_exist = True
+        if task_exist:
+            print(f'Задача "{description}" уже есть в списке, срок выполнения: {task['deadline']}')
+        else:
+            self.tasks.append({'description':description, 'deadline':deadline, 'status':'in_progress'})
+            print(f'Задача "{description}" добавлена в список. Срок выполнения - {deadline}')
+
+    def task_done(self, description):
+        task_exist = False
+        for task in self.tasks:
+            if task['description'] == description:
+                task['status'] = 'done'
+                task_exist = True
+        if task_exist: print(f'Задача "{description}" выполнена!')
+        else: print(f'Задача "{description}" не найдена')
+
+    def show_tasks(self):
+        print('Текущие задачи:')
+        for task in self.tasks:
+            if task['status'] == 'in_progress': print(f'{task['description']}: срок выполнения - {task['deadline']}')
+
